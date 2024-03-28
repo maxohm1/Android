24c84a3317f9c5b3d1d9ba91143d5a54dd7796be
maxohm1 <149907836+maxohm1@users.noreply.github.com>
2024-03-28 16:01:15 +0530

    Create input.kt

---
+++ input.kt
@@ -0,0 +1,10 @@
+fun main() {
+    // Prompt the user for input
+    print("Enter something: ")
+
+    // Read the input as a String
+    val userInput: String? = readLine()
+
+    // Print the input
+    println("You entered: $userInput")
+}
